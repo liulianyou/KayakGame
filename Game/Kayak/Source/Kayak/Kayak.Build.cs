a789ebaf5655e4aaee9459e17b44ea34368bbeaf
Liulianyou <cumt_lly@163.com>
2021-11-28 17:20:19 +0800

    Add files via upload

--- Game/Kayak/Source/Kayak/Kayak.Build.cs
+++ Game/Kayak/Source/Kayak/Kayak.Build.cs
@@ -1,6 +1,8 @@
 // Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
 
 using UnrealBuildTool;
+using System.Collections.Generic;
+using System.IO;
 
 public class Kayak : ModuleRules
 {
@@ -8,21 +10,35 @@ public class Kayak : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore",
-        "SlateCore", "Slate", "UMG"});
+		PublicDependencyModuleNames.AddRange(new string[] {
+            "Core", "CoreUObject", "Engine", "InputCore",
+            "SlateCore", "Slate", "UMG"});
 
-        PublicIncludePaths.AddRange(new string[] {
-            "Kayak/Public/UI"
-        });
+        PublicIncludePaths.AddRange(GetPublicIncludePath());
 
-        PrivateIncludePaths.AddRange(new string[] {
-            "Kayak/Public/Pawn",
-            "Kayak/Public/UI",
-            "Kayak/Public/Controller",
-            "Kayak/Public/GameMode",
-            "Kayak/Public/GamePlayClass",
-        });
+        PrivateIncludePaths.AddRange(GetPrivateIncludePath());
+    }
 
+    public string[] GetPublicIncludePath()
+    {
+       return Directory.GetDirectories(ModulePath);
 
+    }
+
+    public string[] GetPrivateIncludePath()
+    {
+        return new string[] {
+        };
+    }
+
+    private string ModulePath
+    {
+        get
+        {
+            //return Path.GetDirectoryName(RulesCompiler.GetModuleFilename(this.GetType().Name));
+            string ModuleFilename = UnrealBuildTool.RulesCompiler.GetFileNameFromType(GetType());
+            string ModuleBaseDirectory = Path.GetDirectoryName(ModuleFilename);
+            return ModuleBaseDirectory;
+        }
     }
 }
